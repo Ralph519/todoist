972aa339ecf067529e39549d972b94c42b2202b8
r.toledo <r.toledo@archriodon.net>
2019-09-07 14:14:41 +0400

    finished from the tutorial

---
+++ src/__test__/Checkbox.spec.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import { Checkbox } from '../components/Checkbox'
+import { jsxEmptyExpression } from '@babel/types';
+
+beforeEach(cleanup);
+
+jest.mock('../firebase', () => ({
+    
+}))
